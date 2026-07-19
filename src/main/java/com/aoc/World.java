03b2c914f1a98a85d64a457c6bca278f127a8699
Sma1lo <heee58411@gmail.com>
2026-07-19 19:17:43 +0300

    chore: add world generate

--- src/main/java/com/aoc/World.java
+++ src/main/java/com/aoc/World.java
@@ -1,22 +1,52 @@
 package com.aoc;
 
+
+import java.util.Random;
+
 public class World {
     static final int WIDTH = 40;
     static final int HEIGHT = 20;
-    static int [][] grid = new int [HEIGHT][WIDTH];
+    static Random rand = new Random();
+    static TerrainType [][] grid = new TerrainType[HEIGHT][WIDTH];
 
     public static void init() {
-        generate();
+        fillWater();
+        fillGround();
+        generateGrid();
     }
 
-    public static void generate(){
+    public static void generateGrid(){
+
         StringBuilder sb = new StringBuilder();
         for(int y = 0; y < HEIGHT; y++){
             for(int x = 0; x < WIDTH; x++){
-                sb.append("0");
+                if(grid[y][x] == TerrainType.WATER){
+                    sb.append("~");
+                }
+                else{
+                    sb.append("0");
+                }
             }
             sb.append("\n");
         }
         System.out.println(sb.toString());
+    }
+
+    public static void fillWater(){
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                grid[y][x] = TerrainType.WATER;
+            }
+        }
+    }
+
+    public static void fillGround(){
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                if(rand.nextInt(100)< 50){
+                    grid[y][x] = TerrainType.GROUND;
+                }
+            }
+        }
     }
 }
